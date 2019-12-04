1ef07707126b471cf631a75aebb09fb162ff33c1
Allen Faure <afaure6@gmail.com>
2019-12-03 20:15:20 -0600

    2.2!

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,31 +1,45 @@
 
-pub fn solve() {
-    let mut data = super::get_data_as_ints("02_opcodes.txt");
-    data[1] = 12;
-    data[2] = 2;
-    for i in 0..data.len()/4 {
-        let index = i * 4;
-        println!("op: {}, data: [{}, {}], loc: {}", data[index], data[index+1], data[index+2], data[index+3]);
-        match data[index] {
-            99 => {
-                println!("done!")
-            },
+fn evaluate(data: &Vec<i64>, noun: i64, verb: i64) -> i64 {
+    let mut mem = data.clone();
+    mem[1] = noun;
+    mem[2] = verb;
+    let mut ptr = 0;
+    while ptr < mem.len() {
+        let op = mem[ptr];
+        match op {
+            99 => return mem[0],
             1 => {
-                let loc0 = data[index + 1] as usize;
-                let loc1 = data[index + 2] as usize;
-                let loc = data[index + 3] as usize;
-                data[loc] = data[loc0] + data[loc1];
-                println!("result: {}", data[loc])
+                let loc0 = mem[ptr + 1] as usize;
+                let loc1 = mem[ptr + 2] as usize;
+                let loc = mem[ptr + 3] as usize;
+                mem[loc] = mem[loc0] + mem[loc1];
             },
             2 => {
-                let loc0 = data[index + 1] as usize;
-                let loc1 = data[index + 2] as usize;
-                let loc = data[index + 3] as usize;
-                data[loc] = data[loc0] * data[loc1];
-                println!("result: {}", data[loc])
+                let loc0 = mem[ptr + 1] as usize;
+                let loc1 = mem[ptr + 2] as usize;
+                let loc = mem[ptr + 3] as usize;
+                mem[loc] = mem[loc0] * mem[loc1];
             },
-            _ => panic!("invalid opcode {}", data[index])
+            _ => panic!("invalid opcode {}", mem[ptr])
+        }
+        ptr += 4;
+    }
+    panic!("Reached end of memory without stopping")
+}
+
+
+pub fn solve() {
+    let data = super::get_data_as_ints("02_opcodes.txt");
+    println!("Part 1: Result is {}", evaluate(&data, 12, 2));
+    for noun in 0..100 {
+        for verb in 0..100 {
+            match evaluate(&data, noun, verb) {
+                19690720 => {
+                    println!("Done. Noun = {}, Verb = {}", noun, verb);
+                    break
+                },
+                _ => continue,
+            }
         }
     }
-    println!("Part 1: final result is {}", data[0])
 }
